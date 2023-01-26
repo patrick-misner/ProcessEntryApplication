7c4eacc29ba58d682bfad25537e58941520c5d26
Patrick Misner <misner.patrick@gmail.com>
2023-01-26 03:27:47 -0700

    backend is being stupid

--- ProcessEntryApp/Program.cs
+++ ProcessEntryApp/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,7 +14,12 @@ if (!app.Environment.IsDevelopment())
   app.UseExceptionHandler("/Home/Error");
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
   app.UseHsts();
-
+  app.UseSwagger();
+  app.UseSwaggerUI(options =>
+{
+  options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+  options.RoutePrefix = string.Empty;
+});
 }
 
 app.UseHttpsRedirection();
@@ -20,7 +28,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
