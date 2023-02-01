75a218a810d170b23a065ded00a7a1676127cb52
Patrick-Misner <misner.patrick@gmail.com>
2023-02-01 03:25:30 -0700

    readme update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+![alt text](img/reactandapiauth.gif "Login")
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- processentryappclient/src/auth0-provider-with-navigate.tsx
+++ processentryappclient/src/auth0-provider-with-navigate.tsx
@@ -14,12 +14,13 @@ export const Auth0ProviderWithNavigate = ({
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
   const onRedirectCallback = (appState?: AppState) => {
     navigate(appState?.returnTo || window.location.pathname);
   };
 
-  if (!(domain && clientId && redirectUri)) {
+  if (!(domain && clientId && redirectUri && audience)) {
     return null;
   }
 
@@ -28,7 +29,8 @@ export const Auth0ProviderWithNavigate = ({
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: redirectUri,
+        audience: audience,
+        redirect_uri: redirectUri
       }}
       onRedirectCallback={onRedirectCallback}
     >
