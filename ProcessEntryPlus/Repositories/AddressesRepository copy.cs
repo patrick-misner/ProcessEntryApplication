321955c257b4e93f5222b49ac4c47c660c5077e7
Patrick-Misner <misner.patrick@gmail.com>
2023-02-05 19:14:29 -0700

    account

---
+++ ProcessEntryPlus/Repositories/AddressesRepository copy.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using ProcessEntryPlus.Models;
+using MySqlConnector;
+
+
+namespace ProcessEntryPlus.Repositories
+{
+  public class AccountsRepository
+  {
+    private readonly MySqlConnection _db;
+
+    public AccountsRepository(MySqlConnection db)
+    {
+      _db = db;
+    }
+
+    internal Account GetByEmail(string userEmail)
+    {
+      string sql = "SELECT * FROM accounts WHERE email = @userEmail";
+      return _db.QueryFirstOrDefault<Account>(sql, new { userEmail });
+    }
+
+    internal Account GetById(string id)
+    {
+      string sql = "SELECT * FROM accounts WHERE id = @id";
+      return _db.QueryFirstOrDefault<Account>(sql, new { id });
+    }
+
+    internal Account Create(Account newAccount)
+    {
+      string sql = @"
+            INSERT INTO accounts
+              (name, picture, email, id)
+            VALUES
+              (@Name, @Picture, @Email, @Id)";
+      _db.Execute(sql, newAccount);
+      return newAccount;
+    }
+
+    internal Account Edit(Account update)
+    {
+      string sql = @"
+            UPDATE accounts
+            SET 
+              name = @Name,
+              picture = @Picture
+            WHERE id = @Id;";
+      _db.Execute(sql, update);
+      return update;
+    }
+
+    internal Profile GetProfile(string id)
+    {
+      string sql = "SELECT * FROM accounts WHERE id = @id";
+      return _db.QueryFirstOrDefault<Profile>(sql, new { id });
+    }
+  }
+}
+
