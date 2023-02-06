958497a01d10b40097f0ebb7548bc11a0bd328f0
Patrick-Misner <misner.patrick@gmail.com>
2023-02-05 19:17:49 -0700

    accounts

--- ProcessEntryPlus/Controllers/AccountController.cs
+++ ProcessEntryPlus/Controllers/AccountController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ProcessEntryPlus.Models;
-using Microsoft.AspNetCore.Authorization;
+using ProcessEntryPlus.Services;
 
 namespace ProcessEntryPlus.Controllers
 {
@@ -9,29 +8,26 @@ namespace ProcessEntryPlus.Controllers
   public class AccountController : ControllerBase
   {
     private readonly AccountService _accountService;
-    private readonly VaultsService _vServ;
-
-    private readonly VaultKeepsService _vkServ;
 
     public AccountController(AccountService accountService)
     {
       _accountService = accountService;
     }
 
-    [HttpGet]
-    [Authorize]
-    public async Task<ActionResult<Account>> Get()
-    {
-      try
-      {
-        Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
-        return Ok(_accountService.GetOrCreateProfile(userInfo));
-      }
-      catch (Exception e)
-      {
-        return BadRequest(e.Message);
-      }
-    }
+    // [HttpGet]
+    // [Authorize]
+    // public Task<ActionResult<Account>> Get()
+    // {
+    //   try
+    //   {
+    //     // Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+    //     // return Ok(_accountService.GetOrCreateProfile(userInfo));
+    //   }
+    //   catch (Exception)
+    //   {
+    //     // return Task.FromResult(BadRequest(e.Message));
+    //   }
+    // }
 
   }
 
