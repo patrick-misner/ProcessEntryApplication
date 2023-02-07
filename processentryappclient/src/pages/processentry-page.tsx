da1d7db9e9ab2723be42bba79421fa65527f1229
Patrick-Misner <misner.patrick@gmail.com>
2023-02-07 10:53:52 -0700

    process entry view page

--- processentryappclient/src/pages/processentry-page.tsx
+++ processentryappclient/src/pages/processentry-page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { PageLayout } from '../components/page-layout'
-import axios from "axios"
 import { api } from '../services/AxiosService'
 
 const ProcessentryPage = () => {
---
+++ processentryappclient/src/pages/processview-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function ProcessViewPage() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default ProcessViewPage; 
