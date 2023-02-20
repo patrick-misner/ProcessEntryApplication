0cac8cc9ecc6311e4d31ff3d61bae9c018793357
Patrick-Misner <misner.patrick@gmail.com>
2023-02-20 03:26:11 -0700

    asdfasdok

--- processentryappclient/src/pages/processview-page.tsx
+++ processentryappclient/src/pages/processview-page.tsx
@@ -7,7 +7,10 @@ const ProcessviewPage = () => (
       <div className="grid grid-cols-12 gap-1">
         <div className="col-span-12 md:col-span-4" />
         <div className="col-span-12 md:col-span-4">
-          <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button
+            type="button"
+            className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
             SpaceShip
           </button>
         </div>
